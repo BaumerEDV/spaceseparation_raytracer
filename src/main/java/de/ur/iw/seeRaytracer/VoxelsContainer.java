bb290a9e2988a7915da96daa8cda2bfabe72e468
Stefan Erwin Baumer <Stefan-Erwin.Baumer@stud.uni-regensburg.de>
2019-06-07 17:27:33 +0200

    created space separation and test

--- src/main/java/de/ur/iw/seeRaytracer/VoxelsContainer.java
+++ src/main/java/de/ur/iw/seeRaytracer/VoxelsContainer.java
@@ -1,12 +1,92 @@
 package de.ur.iw.seeRaytracer;
 
+import com.google.common.collect.UnmodifiableIterator;
+import org.apache.commons.math3.geometry.euclidean.threed.Vector3D;
+import org.checkerframework.checker.nullness.compatqual.NonNullType;
+
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
 
 public class VoxelsContainer {
 
     private HashMap<VoxelPos, Voxel> voxels;
+    private VoxelBoundingBox boundingBox;
+
+    public VoxelsContainer(Collection<Triangle> trianglesToBeDisplayed) {
+        voxels = new HashMap<>();
+        createVoxelsAndPutContainedTrianglesIntoThem(trianglesToBeDisplayed);
+        createBoundingBoxFromTriangles(trianglesToBeDisplayed);
+    }
+
+    private void createVoxelsAndPutContainedTrianglesIntoThem(Collection<Triangle> triangles) {
+        for (Triangle triangle : triangles) {
+            var voxelsThisBelongsTo = createCollectionOfPositionsThisBelongsTo(triangle);
+            for (VoxelPos pos : voxelsThisBelongsTo) {
+                voxels.putIfAbsent(pos, new Voxel(pos));
+                voxels.get(pos).addTriangle(triangle);
+            }
+        }
+    }
+
+    private void createBoundingBoxFromTriangles(Collection<Triangle> triangles) {
+        ArrayList<VoxelPos> positions = new ArrayList<>();
+
+        for (Triangle triangle : triangles) {
+            for (Vector3D vertex : triangle) {
+                positions.add(VoxelPosFactory.createFromRealSpaceVector(vertex));
+            }
+        }
+
+        VoxelPos[] args = new VoxelPos[positions.size()];
+        for (int i = 0; i < args.length; i++) {
+            args[i] = positions.get(i);
+        }
+
+        boundingBox = new VoxelBoundingBox(args);
 
 
+    }
+
+    private Collection<VoxelPos> createCollectionOfPositionsThisBelongsTo(Triangle triangle) {
+        ArrayList<VoxelPos> result = new ArrayList<>();
+        final int SPACE_DIMENSIONS = 3;
+        VoxelPos[] vertexBounds = new VoxelPos[SPACE_DIMENSIONS];
+        int i = 0;
+        for (Vector3D vertex : triangle) {
+            vertexBounds[i] = VoxelPosFactory.createFromRealSpaceVector(vertex);
+            i++;
+        }
+        VoxelBoundingBox box = new VoxelBoundingBox(vertexBounds);
+        var minX = box.min().getX();
+        var maxX = box.max().getX();
+        var minY = box.min().getY();
+        var maxY = box.max().getY();
+        var minZ = box.min().getZ();
+        var maxZ = box.max().getZ();
+
+        for (int x = minX; x <= maxX; x++) {
+            for (int y = minY; y <= maxY; y++) {
+                for (int z = minZ; z <= maxZ; z++) {
+                    result.add(new VoxelPos(x, y, z));
+                }
+            }
+        }
+
+        return result;
+    }
+
+    public VoxelBoundingBox boundingBoxForTestsOnly() {
+        return boundingBox;
+    }
+
+    public HashMap<VoxelPos, Voxel> voxelsForTestsOnly() {
+        return voxels;
+    }
+
+    /*public List<Triangle> getOrderedListOfVoxelsThatRayIntersects(){
+        throw new ExecutionControl.NotImplementedException("");
+    }*/
 
 
 }
--- src/test/java/de/ur/iw/seeRaytracer/VoxelsContainerTest.java
+++ src/test/java/de/ur/iw/seeRaytracer/VoxelsContainerTest.java
@@ -1,18 +1,61 @@
 package de.ur.iw.seeRaytracer;
 
+import org.apache.commons.math3.geometry.euclidean.threed.Vector3D;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class VoxelsContainerTest {
 
+    private VoxelsContainer vc;
+
     @org.junit.jupiter.api.BeforeEach
-    void setUp(){
+    void setUp() {
+        Triangle[] triangles = new Triangle[8];
+        int i = 0;
+        for (int x = -1; x < 1; x++) {
+            for (int y = -1; y < 1; y++) {
+                for (int z = -1; z < 1; z++) {
+                    Vector3D v =
+                            new Vector3D(x * Voxel.VOXEL_WIDTH + Voxel.VOXEL_WIDTH / 2,
+                            y * Voxel.VOXEL_WIDTH + Voxel.VOXEL_WIDTH / 2,
+                            z * Voxel.VOXEL_WIDTH + Voxel.VOXEL_WIDTH / 2);
+                    triangles[i] = new Triangle(v, v, v);
+                    i++;
+                }
+            }
+        }
+        vc = new VoxelsContainer(Arrays.asList(triangles));
 
     }
 
     @org.junit.jupiter.api.AfterEach
-    void tearDown(){
+    void tearDown() {
 
     }
 
+    @Test
+    void boundingBoxIsCorrect() {
+        assertEquals(new VoxelPos(-1, -1, -1), vc.boundingBoxForTestsOnly().min());
+        assertEquals(new VoxelPos(0, 0, 0), vc.boundingBoxForTestsOnly().max());
+    }
 
+    @Test
+    void correctNumberOfTrianglesIsInVoxels() {
+        assertEquals(8, vc.voxelsForTestsOnly().keySet().size());
+        int[] c = new int[]{-1, 0};
+        for (int x = 0; x < 2; x++) {
+            for (int y = 0; y < 2; y++) {
+                for (int z = 0; z < 2; z++) {
+                    assertTrue(vc.voxelsForTestsOnly().containsKey(new VoxelPos(c[x], c[y], c[z])));
+                    assertEquals(1, vc.voxelsForTestsOnly().get(new VoxelPos(c[x], c[y], c[z])).containedTriangles().size());
+                }
+            }
+        }
+    }
 
 
 }
